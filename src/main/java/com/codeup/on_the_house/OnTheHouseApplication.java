3949407838efa3754b8bbd0bd2ba0fac83a40b89
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-06-06 12:22:39 -0500

    first commit

---
+++ src/main/java/com/codeup/on_the_house/OnTheHouseApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.on_the_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnTheHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnTheHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/on_the_house/OnTheHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.on_the_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTheHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
