d6f6ba8b80f186f45ce7f29ed0dccc0544e23fb7
James Woodward <jamesrobertwoodward@gmail.com>
2022-06-06 14:42:58 -0500

    add db_create.sql and setup db with users table and posts table

---
+++ src/main/resources/scripts/db_create.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS oth_db;
+CREATE DATABASE IF NOT EXISTS oth_db;
+
+USE oth_db;
+CREATE TABLE users
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(60) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    phone_number VARCHAR(13) NOT NULL,
+    address VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE posts
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    item_name VARCHAR(70) NOT NULL,
+    description TEXT NOT NULL,
+    item_photo VARCHAR(150) NOT NULL,
+    post_expiry_date DATE NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
