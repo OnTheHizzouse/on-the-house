264c83c7172b6dfff24289525c8bcf5680552d3d
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-06-07 13:32:28 -0400

    front end commits

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -1,14 +1,22 @@
 export default function Home(props) {
     console.log("The frontend did it. HER FAULT");
+// language=html
     return `
         <header>
-            <h1>Home Page</h1>
+            <h1></h1>
         </header>
         <main>
-            <div>
-                <p>
-                    This is the home page text.
-                </p>    
+            <div id="box" >
+            <div class="card" style="width: 40rem;">
+  <div class="card-body">
+    <h5 class="card-title">Registration</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+             
             </div>
         </main>
     `;
