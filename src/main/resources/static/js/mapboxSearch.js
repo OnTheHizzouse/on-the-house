c22903fbc1451a057592bc521101be559bfeed2b
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-06-10 13:32:33 -0500

    add the geoCoder

---
+++ src/main/resources/static/js/mapboxSearch.js
@@ -0,0 +1,51 @@
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWVuZG96YXIiLCJhIjoiY2wwdjB3a3Z4MHRoczNlbnA0ajFiaWttMSJ9.wyy70W2kiLvadQuWo-EBeQ';
+    const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    types: 'country,region,place,postcode,locality,neighborhood,address'
+});
+
+
+    function waitForElm(selector) {
+    return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+    return resolve(document.querySelector(selector));
+}
+
+    const observer = new MutationObserver(mutations => {
+    if (document.querySelector(selector)) {
+    resolve(document.querySelector(selector));
+    observer.disconnect();
+}
+});
+
+    observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
+});
+}
+
+
+
+    waitForElm('#geocoder').then((elm) => {
+    console.log('Element is ready');
+    console.log(elm.textContent);
+    geocoder.addTo('#geocoder');
+});
+
+
+
+    // Get the geocoder results container.
+    const results = document.getElementById('result');
+
+    // Add geocoder result to container.
+    geocoder.on('result', (e) => {
+    results.innerText = JSON.stringify(e.result, null, 2);
+});
+
+    // Clear results container when search is cleared.
+    geocoder.on('clear', () => {
+    results.innerText = '';
+});
+
