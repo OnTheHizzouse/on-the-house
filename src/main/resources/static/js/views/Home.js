e2444b61f30a8b3dc415c778c1483fe73fa7608b
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-06-14 15:13:39 -0500

    create a post

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -32,7 +32,7 @@ export default function Home(props) {
 <img id="OTH" src="officialPhoto.png" alt="">
 <!--todo:Delete this div later-->
 
-${createPostModal()}
+${createPostModal(props.user.username)}
 
 <h4>Hello, ${props.user.username}</h4>
 <div class="containers">
@@ -65,7 +65,6 @@ ${postCards(props.posts)}
 
 </div>
 
-
 <script>
     // TO MAKE THE MAP APPEAR YOU MUST
     // ADD YOUR ACCESS TOKEN FROM
@@ -85,15 +84,19 @@ ${postCards(props.posts)}
     });
 
     document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-    
+   
 </script>
 </body>
-
-
-
-
     `;
 }
+
+// todo add to router
+export function homepageEvent() {
+    savePostEventListener();
+}
+
+savePostEventListener();
+
 
 function postCards(posts) {
     //language=HTML
@@ -125,34 +128,92 @@ function postCards(posts) {
     return htmlCard
 }
 
-function createPostModal() {
+function createPostModal(username) {
     //language=HTML
-
     let htmlModal = ``
-
     htmlModal += `
-
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#postModal">
-  Launch demo modal
+Create Post
 </button>
 
 <div class="modal" tabindex="-1" id="postModal">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title">Create a New Post</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Modal Test </p>
+<!--      todo: get better name-->
+      <label for="item-name"><b>Item:</b></label>
+      <input class="inputFields" placeholder="Enter Item name" name="item-name" id="create-item-name"  required>
+      <br>
+       <label for="description"><b>Describe the item to share:</b></label>
+      <textarea class="inputFields " placeholder="Description of Item" name="description" id="create-description" required></textarea>
+      <br>
+<!--      todo change this to take in a photo-->
+       <label for="photo"><b>photo:</b></label>
+      <input class="inputFields" placeholder="this will be change later" name="photo" id="create-photo"  required>
+      <br>
+<!--     todo possible calender integration -->
+       <label for="date"><b>Post Expiration Date:</b></label>
+      <input class="inputFields" placeholder="yyyy-mm-dd" name="date" id="create-expire-date"  required>
+      <br>
+       <label for="quantity"><b>Quantity:</b></label>
+      <input class="inputFields" placeholder="Enter the number available" name="quantity" id="create-quantity"  required>
+      <br>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button> 
+<!--          todo make sure the inputfields are not null-->
+        <button type="button" class="btn btn-primary"  data-id="${username}" id="create-post-btn" data-bs-dismiss="modal" >Save Post</button>
       </div>
     </div>
   </div>
 </div>
     `
+    return htmlModal;
+}
 
+function savePostEventListener() {
+    $(document).on('click', '#create-post-btn', function (e) {
+
+        console.log("hello this post on save. not")
+        let currentUser= $('#create-post-btn').data('id');
+        let itemName =  $('#create-item-name').val()
+        let description = $('#create-description').val()
+        let itemPhoto =  $('#create-photo').val()
+        let expiryDate = $('#create-expire-date').val()
+        let quantity =     $('#create-quantity').val()
+        const postReqBody = {
+            itemName: itemName,
+            description: description,
+            itemPhoto: itemPhoto,
+            expiryDate: expiryDate,
+            quantity: quantity
+        }
+        savePostFetch(currentUser, postReqBody)
+        $('#create-item-name').val("")
+        $('#create-description').val("")
+        $('#create-photo').val("")
+        $('#create-expire-date').val("")
+        $('#create-quantity').val("")
+
+    })
+}
+
+function savePostFetch(username, reqBody){
+    console.log(username)
+    console.log(reqBody)
+    const options = {
+        headers: {
+            "Content-Type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify(reqBody)
+    }
+    fetch(`http://localhost:8080/api/posts/${username}`, options)
+        .then(res => res.json())
+        .then(console.log('this post has been created'))
+        .catch(err => console.log(err))
 }
