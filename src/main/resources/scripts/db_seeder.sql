a0d404533f893cd46635180a346a5cde2f0d4714
James Woodward <jamesrobertwoodward@gmail.com>
2022-06-15 11:50:02 -0500

    adjust seeder script to check for returning multiple items for finding by item name, confirmed working on backend

--- src/main/resources/scripts/db_seeder.sql
+++ src/main/resources/scripts/db_seeder.sql
@@ -8,4 +8,5 @@ INSERT INTO posts(user_id, item_name, description, item_photo, expiry_date, quan
 VALUES (3, 'Asparagus', 'Totally not rotten', 'test', '22:06:30', 15),
        (2, 'Cream of Mushroom Soup', 'Still in can', 'test', '22:06:29', 1),
        (2, 'Macaroni and Cheese', 'Homemade and delicious', 'test', '22:06:10', 5),
-       (1, 'Leftover Pizza Slice', 'Midway Pizza, only pineapple', 'test', '22:06:9', 2);
+       (1, 'Leftover Pizza Slice', 'Midway Pizza, only pineapple', 'test', '22:06:9', 2),
+       (2, 'Asparagus', 'Makes your pee stink', 'asparagus', '22:06:25', 1);
