ff1a9b95cb2251d9125d4901934facc4234b29ea
James Woodward <jamesrobertwoodward@gmail.com>
2022-06-20 11:50:01 -0500

    get coordinates for users within 5 miles and add markers to map for those users

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -17,9 +17,18 @@ export default function Home(props) {
                 height: 50vh;
                 border: .5rem solid #6a9f5a;
             }
+            .marker {
+                background-image: url('js/views/img/recycleLogo.png');
+                background-size: cover;
+                width: 50px;
+                height: 50px;
+                border-radius: 50%;
+                cursor: pointer;
+            }
         </style>
 
         ${getprops(props.posts)}
+        ${getUserProps(props.user)}
 
         <!--TODO: MOVE INPUT FIELD/SEARCHBAR TO WHERE IT NEEDS TO BE-->
 
@@ -27,7 +36,7 @@ export default function Home(props) {
         <div class="d-flex justify-content-center mb-3">
             <div id="map"></div>
         </div>
-<!--      todo  this add some white space on the right problem is the margin-->
+        <!--      todo  this add some white space on the right problem is the margin-->
         <div class="row justify-content-center mb-3">
             <div class="col-4 my-auto">
                 <input class="inputFields" placeholder="Search items..." name="search-by-item-name"
@@ -54,11 +63,13 @@ export default function Home(props) {
 }
 
 var postProps;
+var userProps;
+var usersWithin5Miles = [];
+var geoJson;
 
 // [-79.4512, 43.6568]
 function initMap(lng, lat) {
     waitForElm('#map').then((elm) => {
-        console.log('hello')
         mapboxgl.accessToken = KEY_mapbox;
         const map = new mapboxgl.Map({
             container: 'map',
@@ -67,6 +78,7 @@ function initMap(lng, lat) {
             zoom: 13
         });
         map.scrollZoom.disable();
+        addUserMarkersToMap(geoJson, map);
     })
 }
 
@@ -74,6 +86,10 @@ waitForElm('#OTH').then((elm) => {
     // $('#OTH').attr("src", "../views/img/othNavImg.png")
 })
 
+function getUserProps(props) {
+    userProps = props;
+    return `<span></span>`
+}
 
 function getprops(props) {
     postProps = props;
@@ -91,7 +107,9 @@ export function homepageEvent() {
     cancelBtnEventListener();
     searchPostsByItemNameEventListener();
     startCards(postProps)
-    initMap(-79.4512, 43.6568);
+    initMap(userProps.coordinates.split(',')[1], userProps.coordinates.split(',')[0]);
+    findUsersWithinRange();
+    createGeoJsonForMarkers(usersWithin5Miles);
 }
 
 function clearModalFields() {
@@ -177,4 +195,99 @@ function savePostFetch(username, reqBody) {
     fetch(`http://localhost:8080/api/posts/${username}`, options)
         .then(console.log('this post has been created'))
         .catch(err => console.log(err))
+}
+
+function findUsersWithinRange() {
+    let users = [];
+    let currentUserLng = userProps.coordinates.split(',')[1];
+    let currentUserLat = userProps.coordinates.split(',')[0];
+    for (let i = 0; i < postProps.length; i++) {
+        if (userProps.id !== postProps[i].user.id) {
+            users.push(postProps[i].user);
+        }
+    }
+    let filteredArrayOfUsers = removeDuplicates(users, 'id');
+    for (let i = 0; i < filteredArrayOfUsers.length; i++) {
+        let distance = distanceInMiBetweenEarthCoordinates(currentUserLat, currentUserLng, filteredArrayOfUsers[i].coordinates.split(',')[0], filteredArrayOfUsers[i].coordinates.split(',')[1]);
+        if (distance <= 5) {
+            usersWithin5Miles.push(filteredArrayOfUsers[i]);
+        }
+    }
+    return usersWithin5Miles
+}
+
+function removeDuplicates(originalArray, prop) {
+    var newArray = [];
+    var lookupObject = {};
+
+    for (var i in originalArray) {
+        lookupObject[originalArray[i][prop]] = originalArray[i];
+    }
+
+    for (i in lookupObject) {
+        newArray.push(lookupObject[i]);
+    }
+    return newArray;
+}
+
+function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function distanceInMiBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+    var earthRadiusMi = 3958.75;
+
+    var dLat = degreesToRadians(lat2 - lat1);
+    var dLon = degreesToRadians(lon2 - lon1);
+
+    lat1 = degreesToRadians(lat1);
+    lat2 = degreesToRadians(lat2);
+
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return earthRadiusMi * c;
+}
+
+function createGeoJsonForMarkers(arrayOfUsers) {
+    geoJson = {
+        type: "FeatureCollection",
+        features: []
+    };
+
+    for (let i = 0; i < arrayOfUsers.length; i++) {
+        let lon = parseFloat(arrayOfUsers[i].coordinates.split(',')[0]);
+        let lat = parseFloat(arrayOfUsers[i].coordinates.split(',')[1]);
+        let username = arrayOfUsers[i].username;
+        geoJson.features.push({
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates: [lat, lon]
+            },
+            properties: {
+                title: username
+            }
+        })
+    }
+    return geoJson;
+}
+
+function addUserMarkersToMap(geoJsonData, map) {
+    for (const feature of geoJsonData.features) {
+// create a HTML element for each feature
+        const el = document.createElement('div');
+        el.className = 'marker';
+
+// make a marker for each feature and add it to the map
+        new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates)
+            .setPopup(
+                new mapboxgl.Popup({offset: 25}) // add popups
+                    .setHTML(
+                        `<h3>${feature.properties.title}</h3>`
+                    )
+            )
+            .addTo(map);
+    }
 }
