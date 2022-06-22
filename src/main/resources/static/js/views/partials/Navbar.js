5e49f5f82123f310244d1e00d2219596997a4072
James Woodward <jamesrobertwoodward@gmail.com>
2022-06-22 12:26:39 -0500

    styling work for homepage

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -2,25 +2,27 @@ export default function Navbar(props) {
     //language=HTML
     //todo: change navbar
     return `
-
-<div>
-  <div id="navImg">
-  <img id="OTH" src="js/views/img/othNavImg.png" alt="Oth logo">
-</div>
-        <nav class="navbar navbar-expand-lg" style="background-color: #64AE8B">
+        <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #FBFAF2" id="completeNavbar">
             <div class="container-fluid d-flex flex-column mx-auto align-items-center text-center">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <div>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" href="/home" data-link>HOME</a>
-                        <a class="nav-link" href="/profile" data-link>PROFILE</a>
-                        <a class="nav-link" href="/events"data-link>EVENTS</a>
-                        <a class="nav-link" href="#">ABOUT</a>
+                    <div class="navbar-nav d-flex align-items-center">
+                        <div id="navImg">
+                            <img id="OTH" src="js/views/img/OTHLogo(final).png" alt="Oth logo">
+                        </div>
+                        <a class="nav-link mx-5" href="/home" data-link>HOME</a>
+                        <a class="nav-link mx-5" href="/profile" data-link>PROFILE</a>
+                        <a class="nav-link mx-5" href="/events"data-link>EVENTS</a>
+                        <a class="nav-link mx-5" href="#">ABOUT</a>
                     </div>
                 </div>
             </div>
         </nav>
+        <div id="navbarSpace"></div>
     `;
+
 }
+
