c1a103700ebffb336200dbc482e52e7f272c9cf0
Johnny Martinez <mrtnz.jn@gmail.com>
2022-06-23 14:30:24 -0500

    Created createEvent methods in EventController and EventService, createEvent tested successfully.

--- src/main/java/com/codeup/on_the_house/web/EventController.java
+++ src/main/java/com/codeup/on_the_house/web/EventController.java
@@ -2,6 +2,7 @@ package com.codeup.on_the_house.web;
 
 
 import com.codeup.on_the_house.data.Event;
+import com.codeup.on_the_house.dto.CreateEventDTO;
 import com.codeup.on_the_house.service.EventService;
 import com.codeup.on_the_house.service.PostService;
 import com.codeup.on_the_house.service.UserService;
@@ -36,6 +37,14 @@ public class EventController {
     public Event getById(@PathVariable Long id){
         System.out.println("Event with ID of: " + id + " retrieved.");
         return eventService.getEventById(id);
+    }
+
+    @PostMapping("{username}/{requesterName}/{postId}")
+    private void addNewEvent(@RequestBody CreateEventDTO createEventDTO, @PathVariable String username,
+                             @PathVariable String requesterName, @PathVariable Long postId){
+        Event newEvent = new Event();
+        eventService.createEvent(createEventDTO, newEvent, username, requesterName, postId);
+        System.out.println("New event created by: " + requesterName + "for post with ID of: " + postId + "for user: " + username);
     }
 
 
