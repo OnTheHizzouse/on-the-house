77a18ebb93e07a11d9fd2c58e1472018dce250d1
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-06-23 15:20:23 -0500

    produce pic updates

--- src/main/resources/static/js/views/About.js
+++ src/main/resources/static/js/views/About.js
@@ -4,15 +4,19 @@ export default function About() {
     return `
         <title>On the house</title>
         <body>
+        
         <div id="about-me-card" class="row justify-content-center">
-            <div class="card mb-3">
-                <img src="js/views/img/produce-cropped.jpg" class="card-img" alt="...">
-            </div>
-            <h5 class="card-title">About Us</h5>
-            <p class="card-text">As a community sharing application we will foster the exchange of goods with the goal
-                of reducing food waste and food insecurity within local communities. We hope to create a comfortable
-                and welcoming environment for all users that will facilitate and encourage community interaction and
-                cooperation.</p>
+                <img src="js/views/img/produce-cropped.jpg" class="card-img-produce justify-content-center" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">What are we all about?</h5>
+                    <p class="card-text">As a community sharing application we will foster the
+                        exchange of goods with the goal
+                        of reducing food waste and food insecurity within local communities. We hope to create a
+                        comfortable
+                        and welcoming environment for all users that will facilitate and encourage community interaction
+                        and
+                        cooperation.</p>
+                </div>
         </div>
 
         <div id="photo-cards" class="row justify-content-center g-4">
@@ -27,7 +31,7 @@ export default function About() {
             </div>
 
             <div class="card col-4" style="width: 15rem;">
-                <img src="js/views/img/ryan.jpg" class="card-img-top" alt="...">
+                <img src="js/views/img/ryan.jpeg" class="card-img-top" alt="...">
                 <p>Ryan Mendoza</p>
             </div>
 
