492f772d1035d85624301ad59973c90453094f09
Tyreik Williams <tyreik.mm.williams@gmail.com>
2022-06-24 11:29:07 -0400

    profile cards modified

--- src/main/resources/static/js/views/Profile.js
+++ src/main/resources/static/js/views/Profile.js
@@ -1,6 +1,5 @@
 import {getHeaders} from "../auth.js";
 import {myPostCards, postCards} from "./partials/postCards.js";
-import {myFooter} from "./partials/footer.js";
 
 const postUrl = "http://localhost:8080/api/posts"
 let postId = "";
@@ -14,122 +13,49 @@ export default function Profile(props) {
 // language=html
     return `
         <body>
-        <!-- ************************************************************************************* PROFILE PHOTO STARTED -->
-
-        <div class="card mx-auto mt-2 col-6 justify-content-center">
-            <div class="card-body">
-                <div class="d-flex flex-column mx-auto align-items-center text-center">
-                    <img src="pieguy2.png" alt="Admin"
-                         class="rounded-circle" width="150">
-                    <div class="mt-3">
-                        <h4>Pie Guy Jr</h4>
-                        <p class="text-muted font-size-sm">Dallas,Tx</p>
-                        <button class="btn btn-outline-primary">Message</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- ************************************************************************************* PROFILE PHOTO STOP -->
-
-        <!-- ************************************************************************************* PROFILE LINKS START -->
-        <div class="card mx-auto mb-2 mt-2 col-10 justify-content-center">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">
-                        Bio:
-                    </h6>
-                    <span id="bio" class="text-secondary">........</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                             viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round"
-                             class="feather feather-twitter mr-2 icon-inline text-info">
-                            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                        </svg>
-                        Twitter
-                    </h6>
-                    <span class="text-secondary">@PieGuy</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                             viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round"
-                             class="feather feather-instagram mr-2 icon-inline text-danger">
-                            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                        </svg>
-                        Instagram
-                    </h6>
-                    <span class="text-secondary">@PieGuy</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                             viewBox="0 0 24 24"
-                             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                             stroke-linejoin="round"
-                             class="feather feather-facebook mr-2 icon-inline text-primary">
-                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                        </svg>
-                        Facebook
-                    </h6>
-                    <span class="text-secondary">@PieGuy</span>
-                </li>
-            </ul>
-
-            <div class="card-body">
+        <div class="d-flex justify-content-center">
+            <div style="width: 80%">
                 <div class="row">
                     <div class="col-sm-3">
-                        <h6 class="mb-0">Full Name</h6>
+                        <h5 class="mb-0">Full Name</h5>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                        Pie Guy Jr.
+                        <p>${profileProps.user.firstName} ${profileProps.user.lastName}</p>
                     </div>
                 </div>
                 <hr>
                 <div class="row">
                     <div class="col-sm-3">
-                        <h6 class="mb-0">Email</h6>
+                        <h5 class="mb-0">Email</h5>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                        Pieguyjr@email.com
+                        ${profileProps.user.email}
                     </div>
                 </div>
                 <hr>
                 <div class="row">
                     <div class="col-sm-3">
-                        <h6 class="mb-0">Mobile</h6>
+                        <h5 class="mb-0">Phone Number</h5>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                        (757) 803-3233
+                        ${profileProps.user.phoneNumber}
                     </div>
                 </div>
                 <hr>
                 <div class="row">
                     <div class="col-sm-3">
-                        <h6 class="mb-0">Address</h6>
+                        <h5 class="mb-0">Address</h5>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                        721 Navarro St #300, Dallas, TX 78111
+                        ${profileProps.user.address}
                     </div>
                 </div>
             </div>
         </div>
-        <!-- ************************************************************************************* PROFILE INFO END -->
-        <div>
 
-            <div id="profileCards" class="row justify-content-center">
-                ${myPostCards(profileProps.posts)}
-            </div>
-            
+        <div id="profileCards" class="row mt-4 justify-content-center">
+            ${myPostCards(profileProps.posts)}
         </div>
-        <!--                ********************************************-->
 
         </body>
     `;
