76a2d986a726d08bb871cf99eaf740f209e16947
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-06-30 13:06:47 -0500

    added doc rob and adjusted footer

--- src/main/resources/static/js/views/partials/footer.js
+++ src/main/resources/static/js/views/partials/footer.js
@@ -1,16 +1,19 @@
 export function myFooter() {
     //language=HTML
     return `
+        <body>
         <footer class="bg-light text-center text-lg-start" id="footer-final">
             <div class="text-center text-light p-3" style="background-color: #2287bb;">
                 <img src="js/views/img/recycle-img.png" class="recycle-img">
                <div>
-                © 2022 Copyright:
-                <a class="text-light" href="https://codeup.com/" target="_blank">  On The House  </a>|
-                <a class="text-light" href="https://www.feedingamerica.org/find-your-local-foodbank" target="_blank">  Find a Food-bank Near You  </a>|
-                <a class="text-light" href="js/views/img/OTH_TOS.png" target="_blank">  Terms Of Service  </a>
+                   © 2022 Copyright:
+                <div><a class="text-light" href="https://codeup.com">Join us at Codeup</a>|
+                <a class="text-light" href="https://www.feedingamerica.org/find-your-local-foodbank">Find a Food Bank Near You</a>|
+                <a class="text-light" href="js/views/img/OTH_TOS.pdf" download>Terms Of Service</a>
+                </div>
             </div>
             </div>
         </footer>
+        </body>
     `
 }
